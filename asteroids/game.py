e62477b6758c83280326fb0e237c7f983a729611
C <chrismatusemail@gmail.com>
2021-07-10 20:15:48 +0100

    Add models.

--- asteroids/game.py
+++ asteroids/game.py
@@ -1,10 +1,12 @@
 import pygame
+from utils import load_sprite
 
 
 class Asteroids:
     def __init__(self):
         self._init_pygame()
         self.screen = pygame.display.set_mode((800, 600))
+        self.background = load_sprite("space", False)
 
     def main_loop(self):
         while True:
@@ -14,7 +16,7 @@ class Asteroids:
 
     def _init_pygame(self):
         pygame.init()
-        pygame.display.set_caption("Space Rocks")
+        pygame.display.set_caption("Asteroids")
 
     def _handle_input(self):
         for event in pygame.event.get():
@@ -25,5 +27,5 @@ class Asteroids:
         pass
 
     def _draw(self):
-        self.screen.fill((0, 0, 255))
+        self.screen.blit(self.background, (0, 0))
         pygame.display.flip()
