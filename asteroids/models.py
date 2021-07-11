ac2a7a3a3479184ac2a34cd1dc5fe60c35f3d326
C <chrismatusemail@gmail.com>
2021-07-11 13:16:45 +0100

    Add rotation and asteroid.

--- asteroids/models.py
+++ asteroids/models.py
@@ -1,4 +1,10 @@
 from pygame.math import Vector2
+from pygame.transform import rotozoom
+from utils import load_sprite, wrap_position, slow_down_velocity
+from random import randint, choice
+
+UP = Vector2(0, -1)
+
 
 class GameObject:
     def __init__(self, position, sprite, velocity):
@@ -11,9 +17,63 @@ class GameObject:
         blit_position = self.position - Vector2(self.radius)
         surface.blit(self.sprite, blit_position)
 
-    def move(self):
-        self.position = self.position + self.velocity
+    def move(self, surface):
+        self.position = wrap_position(self.position + self.velocity, surface)
 
     def collides_with(self, other_obj):
         distance = self.position.distance_to(other_obj.position)
         return distance < self.radius + other_obj.radius
+
+
+class Asteroid(GameObject):
+    direction = Vector2(UP)
+    sign = choice([num for num in range(-5, 5) if num])
+
+    def __init__(self, position):
+        sprite = load_sprite("asteroid")
+        super().__init__(position, sprite, Vector2(0))
+
+    def move(self, surface):
+        self.direction.rotate_ip(self.sign)
+        super().move(surface)
+
+    def draw(self, surface):
+        angle = self.direction.angle_to(UP)
+        print("asteroid angle to up", angle)
+        rotated_surface = rotozoom(self.sprite, angle, 1.0)
+        rotated_surface_size = Vector2(rotated_surface.get_size())
+        blit_position = self.position - rotated_surface_size * 0.5
+        surface.blit(rotated_surface, blit_position)
+
+
+class Spaceship(GameObject):
+    direction = Vector2(UP)
+    MANOEUVRABILITY = 7
+    ACCELERATION = 0.25
+
+    def __init__(self, position, sprite=None):
+        if not sprite:
+            sprite = load_sprite("spaceship")
+
+        super().__init__(position, sprite, Vector2(0))
+
+    def rotate(self, clockwise=True):
+        sign = 1 if clockwise else -1
+        angle = self.MANOEUVRABILITY * sign
+        print("angle", angle)
+        self.direction.rotate_ip(angle)
+
+    def draw(self, surface):
+        angle = self.direction.angle_to(UP)
+        rotated_surface = rotozoom(self.sprite, angle, 1.0)
+        rotated_surface_size = Vector2(rotated_surface.get_size())
+        blit_position = self.position - rotated_surface_size * 0.5
+        surface.blit(rotated_surface, blit_position)
+
+    def accelerate(self):
+        self.velocity += self.direction * self.ACCELERATION
+
+    def move(self, surface):
+        slow_down_velocity(self.velocity)
+
+        super().move(surface)
--- asteroids/utils.py
+++ asteroids/utils.py
@@ -1,4 +1,6 @@
 from pygame.image import load
+from pygame.math import Vector2
+
 
 def load_sprite(name, with_alpha=True):
     path = f"asteroids/assets/sprites/{name}.png"
@@ -8,3 +10,17 @@ def load_sprite(name, with_alpha=True):
         return loaded_sprite.convert_alpha()
     else:
         return loaded_sprite.convert()
+
+
+def wrap_position(position, surface):
+    x, y = position
+    w, h = surface.get_size()
+    return Vector2(x % w, y % h)
+
+
+def slow_down_velocity(vel):
+    def slowed(point):
+        return 0 if 0.01 > point > -0.01 else point * 0.99
+
+    vel[0] = slowed(vel[0])
+    vel[1] = slowed(vel[1])
